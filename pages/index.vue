58eaa0d71ffc2e029b016b0d4925e4830df1f276
David Oyinbo <dev.davexoyinbo@gmail.com>
2021-11-12 11:57:24 +0100

    Key features

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <hero-page />
     <landing-page-info />
     <landing-page-upload />
+    <landing-page-key-features
+      class="bg-gradient-to-t from-transparent to-[rgba(119,195,96,0.1)]"
+    />
   </div>
 </template>
 
@@ -10,10 +13,16 @@
 import Vue from 'vue'
 import HeroPage from '~/components/landing/widgets/HeroPage.vue'
 import LandingPageInfo from '~/components/landing/widgets/LandingPageInfo.vue'
+import LandingPageKeyFeatures from '~/components/landing/widgets/LandingPageKeyFeatures.vue'
 import LandingPageUpload from '~/components/landing/widgets/LandingPageUpload.vue'
 
 export default Vue.extend({
-  components: { HeroPage, LandingPageInfo, LandingPageUpload },
+  components: {
+    HeroPage,
+    LandingPageInfo,
+    LandingPageUpload,
+    LandingPageKeyFeatures,
+  },
   layout: 'landing',
 })
 </script>
