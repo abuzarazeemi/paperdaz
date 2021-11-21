e708ef77d881bd964cc9abecfd3054160efd94bc
David Oyinbo <dev.davexoyinbo@gmail.com>
2021-11-21 14:32:51 +0100

    Paid profile card

---
+++ components/profile/PaidProfileCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div
+      class="rounded-2xl bg-white py-6 px-6 flex items-center justify-center"
+    >
+      <div class="flex flex-col items-center">
+        <h3
+          class="
+            uppercase
+            text-paperdazgray-500
+            font-semibold
+            text-center
+            mb-4
+            text-xl
+          "
+        >
+          Paid Account
+        </h3>
+        <div class="h-40 w-40 rounded-2xl border-2 border-[#B7EF94] p-1 mb-3">
+          <img
+            src="http://greginhollywood.com/wordpress/wp-content/uploads/ee92ba015561f1657763d72c60b87013.jpg"
+            class="rounded-xl w-full h-full"
+            alt=""
+          />
+        </div>
+        <p class="text-lg font-semibold mb-2">Apple Dental</p>
+      </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2 mt-4 text-[#6C6777]">
+      <div class="bg-white rounded-lg px-2 py-4">
+        <barcode-icon class="w-full h-8 text-paperdazgreen-300 mb-2" />
+        <p class="text-center text-[10px] font-medium">Download</p>
+      </div>
+      <div class="bg-white rounded-lg px-2 py-4">
+        <share-icon class="w-full h-8 text-paperdazgreen-300 mb-2" />
+        <p class="text-center text-[10px] font-medium">Share</p>
+      </div>
+      <div class="bg-white rounded-lg px-2 py-4">
+        <building-icon class="w-full h-8 text-paperdazgreen-300 mb-2" />
+        <p class="text-center text-[10px] font-medium">View Profile</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BarcodeIcon from '../svg-icons/BarcodeIcon.vue'
+import BuildingIcon from '../svg-icons/BuildingIcon.vue'
+import ShareIcon from '../svg-icons/ShareIcon.vue'
+export default Vue.extend({
+  name: 'PaidProfileCard',
+  components: { BarcodeIcon, ShareIcon, BuildingIcon },
+})
+</script>
--- components/profile/ProfileCard.vue
+++ components/profile/ProfileCard.vue
@@ -1,57 +1,39 @@
 <template>
-  <div class="rounded-2xl bg-white py-6 px-6 flex items-center justify-center">
-    <div class="flex flex-col items-center">
-      <h3
-        class="
-          uppercase
-          text-paperdazgray-500
-          font-semibold
-          text-center
-          mb-4
-          text-xl
-        "
-      >
-        Free Account
-      </h3>
-      <div
-        class="circle circle-75 border-4 border-[#B7EF94] mx-auto p-0.5 mb-2"
-      >
-        <div class="circle w-full h-full border-2 border-[#B7EF94] p-1.5">
-          <img
-            src="http://greginhollywood.com/wordpress/wp-content/uploads/ee92ba015561f1657763d72c60b87013.jpg"
-            class="circle w-full h-full"
-            alt=""
-          />
-        </div>
-      </div>
-      <p class="text-lg font-semibold mb-2">{{ user.username }}</p>
-      <div
-        class="
-          border border-paperdazgray-100
-          inline-flex
-          items-center
-          px-2
-          py-0.5
-          text-xs
-          font-semibold
-          rounded-full
-        "
-      >
-        <p class="mr-2">700</p>
-        <single-leaf-no-stalk />
-      </div>
-    </div>
+  <div>
+    <component :is="profileCardComponent" class="h-full" />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import SingleLeafNoStalk from '../svg-icons/SingleLeafNoStalk.vue'
+import FreeProfileCard from '~/components/profile/FreeProfileCard.vue'
+import PaidProfileCard from '~/components/profile/PaidProfileCard.vue'
+
+enum UserTypeEnum {
+  FREE,
+  PAID,
+}
 export default Vue.extend({
   name: 'ProfileCard',
-  components: { SingleLeafNoStalk },
+  components: { FreeProfileCard },
+  data() {
+    return {
+      userType: UserTypeEnum.PAID,
+    }
+  },
   computed: {
-    user(): any {
-      return this.$auth.user
+    profileCardComponent(): any {
+      switch (this.userType) {
+        case UserTypeEnum.FREE:
+          return FreeProfileCard
+        case UserTypeEnum.PAID:
+          return PaidProfileCard
+        default:
+          return {
+            render(createElement: any) {
+              return createElement('h1', 'Not implemented yet')
+            },
+          }
+      }
     },
   },
 })
