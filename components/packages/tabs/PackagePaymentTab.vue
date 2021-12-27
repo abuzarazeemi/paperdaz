a4d80aa4dbef86dd064491ce74d0ecfa6fe9cb7d
Oyinbo David Bayode <dev.davexoyinbo@gmail.com>
2021-12-27 12:29:43 +0100

    Subscribe

---
+++ components/packages/tabs/PackagePaymentTab.vue
@@ -0,0 +1,219 @@
+<template>
+  <div>
+    <!-- Start:: login form -->
+    <form class="" @submit="submit">
+      <message-alert-widget
+        class="mb-7"
+        :message="errorMessage"
+        v-if="errorMessage"
+        :type="'error'"
+      />
+      <div class="form-group">
+        <label class="input-label" for="">Name of card holder</label>
+        <el-input
+          :disabled="loading"
+          placeholder="Name Surname"
+          required
+          v-model="name"
+        />
+      </div>
+      <div class="form-group">
+        <label class="input-label" for="">Card Number</label>
+        <el-input
+          :disabled="loading"
+          placeholder="0000-0000-0000-0000"
+          :value="cardNumberWithDashes"
+          @input="inputCardNumber"
+          required
+        />
+      </div>
+      <div class="grid gap-5 grid-cols-2">
+        <div class="form-group">
+          <label class="input-label" for="">Expiration Date</label>
+          <el-input
+            :disabled="loading"
+            placeholder="MM/YY"
+            required
+            :value="expirationDateWithSlashes"
+            @input="inputExpirationDate"
+          />
+        </div>
+        <div class="form-group">
+          <label class="input-label" for="">CVV</label>
+          <el-input
+            :disabled="loading"
+            placeholder="000"
+            required
+            v-model="cvv"
+            type="password"
+            maxlength="3"
+          />
+        </div>
+      </div>
+
+      <div class="grid place-items-center mt-6">
+        <button
+          class="
+            rounded-lg
+            bg-paperdazgreen-400
+            text-white
+            shadow
+            text-sm
+            h-10
+            px-6
+            disabled:bg-opacity-50
+          "
+          :disabled="loading"
+        >
+          <span class="inline-flex items-center gap-3">
+            <span>Pay via Stripe</span>
+            <transition name="fade" :duration="100">
+              <span v-show="loading" class="animate-spin">
+                <spinner-dotted-icon height="22" width="22" />
+              </span>
+            </transition>
+          </span>
+        </button>
+      </div>
+    </form>
+    <!-- End:: login form -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SpinnerDottedIcon from '~/components/svg-icons/SpinnerDottedIcon.vue'
+import MessageAlertWidget from '~/components/widgets/MessageAlertWidget.vue'
+export default Vue.extend({
+  components: { SpinnerDottedIcon, MessageAlertWidget },
+  name: 'PackagePaymentTab',
+  data() {
+    return {
+      loading: false,
+      errorMessage: '',
+      cardNumberWithDashes: '',
+      name: '',
+      cvv: '',
+      expirationDateWithSlashes: '',
+    }
+  },
+  computed: {
+    cardNumber(): string {
+      return this.cardNumberWithDashes.replace(/-+/g, '')
+    },
+    expirationDateString(): string {
+      return this.expirationDateWithSlashes.split('/').join('') || ''
+    },
+    expYear(): string {
+      return this.expirationDateString.split('').splice(2, 4).join('')
+    },
+    expMonth(): string {
+      return this.expirationDateString.split('').splice(0, 2).join('')
+    },
+  },
+  methods: {
+    inputCardNumber(val: string) {
+      let temp = val.split('-').join('') || '' // Remove dash (-) if mistakenly entered.
+      temp = temp.replace(/[^0-9]+/g, '')
+      let finalVal = (temp.match(/.{1,4}/g) || []).join('-')
+      this.cardNumberWithDashes = finalVal
+    },
+    inputExpirationDate(val: string) {
+      let temp = val.split('/').join('') || '' // Remove dash (/) if mistakenly entered.
+      temp = temp.replace(/[^0-9]+/g, '')
+      console.log(temp)
+      if (temp.length > 4) return
+
+      // Check if the month is greater than 12
+      if (temp.length === 2) {
+        if (Number(temp) > 12) temp = '0' + temp
+        if (Number(temp) == 0) temp = '01'
+      }
+
+      let finalVal = (temp.match(/.{1,2}/g) || []).join('/')
+      this.expirationDateWithSlashes = finalVal
+    },
+    async submit() {
+      event?.preventDefault()
+
+      if (!this.expYear || !this.expMonth) {
+        this.errorMessage = 'Please review the expiration date'
+        return
+      }
+
+      if (this.loading) return
+
+      const userId = (this.$auth.user || { id: '' }).id
+
+      const data = {
+        user_id: userId,
+        card_number: this.cardNumber,
+        exp_year: this.expYear,
+        exp_month: this.expMonth,
+        cvc: this.cvv,
+      }
+
+      this.loading = true
+      this.errorMessage = ''
+
+      let card = {} as any
+
+      try {
+        card = await this.$axios.$post('/card', data).then((response) => {
+          response.data
+        })
+      } catch (error: any) {
+        let message = ''
+        if (
+          error &&
+          error.response &&
+          error.response.data &&
+          error.response.data.message
+        ) {
+          message = error.response.data.message
+        } else {
+          message = 'Server not reachable'
+        }
+        this.errorMessage = message
+        this.loading = false
+      }
+
+      this.$axios
+        .$post('/package/subscribe', {
+          user_id: userId,
+          package_id: '',
+          package_payment_type: 'monthly', //yearly
+        })
+        .then((response) => {
+          debugger
+        })
+        .catch((error) => {
+          debugger
+        })
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+form {
+  @apply p-6 rounded-xl border-4 border-paperdazgreen-400 bg-white;
+  width: 100%;
+  max-width: 580px;
+  margin: 0 auto;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.input-label {
+  @apply font-medium text-sm block;
+
+  & + * {
+    @apply mt-1;
+  }
+}
+
+.form-group {
+  &:not(:last-child) {
+    @apply mb-5;
+  }
+}
+</style>
--- pages/packages.vue
+++ pages/packages.vue
@@ -25,8 +25,8 @@
           class="tab-indicator-circle circle circle-30"
           :class="{ active: tabLevel >= 3 }"
         >
-          <span class="highlight">3. Mail</span>
-          <mail-icon height="20" width="20" />
+          <span class="highlight">3. Payment</span>
+          <payment-card-icon height="20" width="20" />
         </div>
       </div>
       <!-- End:: tab indicator -->
@@ -43,17 +43,19 @@
 <script lang="ts">
 import Vue from 'vue'
 import PackageBasicInfoTabVue from '~/components/packages/tabs/PackageBasicInfoTab.vue'
+import PackagePaymentTabVue from '~/components/packages/tabs/PackagePaymentTab.vue'
 import SelectPackgeTabVue from '~/components/packages/tabs/SelectPackgeTab.vue'
 import MailIcon from '~/components/svg-icons/MailIcon.vue'
+import PaymentCardIcon from '~/components/svg-icons/PaymentCardIcon.vue'
 import UserProfileSolidIcon from '~/components/svg-icons/UserProfileSolidIcon.vue'
 export default Vue.extend({
-  components: { UserProfileSolidIcon, MailIcon },
+  components: { UserProfileSolidIcon, MailIcon, PaymentCardIcon },
   name: 'PackagesLandingPage',
   layout: 'landing',
   auth: false,
   data() {
     return {
-      tabLevel: 1,
+      tabLevel: 3,
     }
   },
 
@@ -64,6 +66,8 @@ export default Vue.extend({
           return SelectPackgeTabVue
         case 2:
           return PackageBasicInfoTabVue
+        case 3:
+          return PackagePaymentTabVue
         default:
           return {
             render(createElement: any) {
