42d3755e8f832e37215a6d828cae09075da07319
Oyinbo David Bayode <dev.davexoyinbo@gmail.com>
2022-01-24 03:24:54 +0100

    Integrated tawk.to

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/element.ts',
+    // '~/plugins/tawkto.client.js',
     { src: '~/plugins/nuxt-hammer.js', ssr: false },
     { src: '~/plugins/bus.js' },
     { src: '~/plugins/outside-click.js' },
--- pages/about.vue
+++ pages/about.vue
@@ -66,6 +66,22 @@ export default Vue.extend({
   name: 'AboutPage',
   layout: 'landing',
   auth: false,
+  head() {
+    return {
+      script: [
+        // ...
+        {
+          hid: 'tawk.to',
+          src: 'https://embed.tawk.to/61ee08389bd1f31184d8e4d8/1fq4t07bg',
+          defer: true,
+        },
+      ],
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    location.href = to.fullPath
+    return
+  },
 })
 </script>
 
